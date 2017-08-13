3c8428292ba3fc66f1877fa08a9f30bc1cf47be7
ewhittley <eric.whittley@gmail.com>
2017-08-13 12:24:51 -0500

    adding route handling for GET and DESTROY baskets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :page_not_found
+
+  protected
+
+    def page_not_found
+    end
 end
--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,4 +1,6 @@
 class BasketsController < ApplicationController
+  before_action :set_basket, only: [:show, :destroy]
+
   def index
     @basket = Basket.new
   end
@@ -15,6 +17,50 @@ class BasketsController < ApplicationController
   end
 
   def show
-
+    if user_signed_in?
+      unless @basket.user == current_user
+        flash[:alert] = "You cannot view a basket that is not yours"
+        redirect_to root_path
+      end
+    else
+      flash[:alert] = "You must sign in before viewing a basket"
+      redirect_to root_path
+    end
   end
+
+  def destroy
+    if user_signed_in?
+      unless @basket.user == current_user
+        flash[:alert] = "You cannot delete a basket that is not yours"
+        redirect_to root_path
+      else
+        if @basket.destroy
+          flash[:success] = "Basket has been deleted"
+          redirect_to baskets_path
+        end
+      end
+    else
+      flash[:alert] = "You must sign in before deleting a basket"
+      redirect_to root_path
+    end
+  end
+
+  protected
+
+    def page_not_found
+      message = "Basket does not exist"
+      flash[:alert] = message
+      redirect_to root_path
+    end
+
+  private
+
+    def basket_params
+      params.require(:basket)
+    end
+
+    def set_basket
+      @basket = Basket.find(params[:id])
+    end
+
 end
