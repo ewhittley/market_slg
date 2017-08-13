13a3fefe82eb6ced3b3e7b2508484f9d753d2a29
ewhittley <eric.whittley@gmail.com>
2017-08-13 09:28:55 -0500

    adding requests testing for baskets show

---
+++ spec/requests/baskets_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe "Baskets", type: :request do
+
+  before do
+    @user1 = User.create(email: "user1@market.com", password: "P@ssword")
+    @user2 = User.create(email: "user2@market.com", password: "P@ssword")
+
+    @basket1 = Basket.create(user: @user1)
+  end
+
+  describe "GET/baskets/:id" do
+    context "with a basket that does not exist" do
+      before { get "/baskets/xxxx" }
+
+      it "handles non-existing basket" do
+        expect(response.status).to eq 302
+      end
+    end
+
+    context "with a basket that belongs to current user" do
+      before do
+        login_as(@user1)
+        get "/baskets/#{@basket1.id}"
+      end
+
+      it "successfully displays basket" do
+        expect(response.status).to eq 200
+      end
+    end
+
+    context "with a basket that does not belong to current user" do
+      before do
+        login_as(@user2)
+        get "/baskets/#{@basket1.id}"
+      end
+
+      it "does not allow user to see basket they are not owner of" do
+        expect(response.status).to eq 302
+      end
+    end
+
+    context "with a user who is not signed in" do
+      before do
+        get "/baskets/#{@basket1.id}"
+      end
+
+      it "does not allow user to see basket they are not owner of" do
+        expect(response.status).to eq 302
+      end
+    end
+  end
+
+  describe "DELETE/baskets/:id" do
+    context "with a basket that does not exist" do
+
+    end
+
+    context "with a basket that belongs to current user" do
+
+    end
+
+    context "with a basket that does not belong to current user" do
+
+    end
+
+    context "with a user who is not signed in" do
+
+    end
+  end
+end
---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Products", type: :request do
+
+  before do
+
+  end
+
+  describe 'GET/products/:id' do
+
+  end
+
+  describe 'DELETE/products/:id' do
+
+  end
+end
