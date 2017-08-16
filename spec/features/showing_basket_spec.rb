1b34362861abd095df9cd78498b50039ee339111
ewhittley <eric.whittley@gmail.com>
2017-08-16 15:52:25 -0500

    adding showing_basket_spec with a user shows an existing basket

---
+++ spec/features/showing_basket_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Showing Baskets" do
+  before do
+    @user1 = User.create(email: "user1@market.com", password: "P@ssword")
+  end
+
+  scenario "a user shows an existing basket" do
+    login_as(@user1)
+    visit "/"
+    click_link "Basket"
+
+    expect(page.current_path).to eq(basket_path(@user1.current_basket.id))
+  end
+
+end
